6640a87b06d06ea9a1f79265d6fa8540e4f311cf
KenLi8504 <kli22@stuy.edu>
2021-10-05 00:56:32 -0400

    Finally found my mistake

--- systems07.c
+++ systems07.c
@@ -7,17 +7,17 @@ int arrayPrintOut(int *array, int length){
   for (int i = 0; i < length - 1; i++){
     printf("%d, ", *(array+i));
   }
-  printf("%d] \n",*(array+length));
+  printf("%d] \n",*(array+length-1));
   return 0;
 }
 
 double averageArray(int *array, int length){
   double counter = 0;
 
-  for (int i = 0; i <= length; i++){
-    printf("%int \n",*(array+i));
+  for (int i = 0; i < length; i++){
+    //printf("%int \n",*(array+i));
     counter = counter + *(array+i);
-    printf("The current sum is %lf \n", counter);
+    //printf("The current sum is %lf \n", counter);
   }
   return (counter/length);
 }
@@ -33,17 +33,23 @@ int main(){
   int test[10];
   int copy[10];
   srand(time(NULL));
-  test[0] = 0;
-  for (int i = 1; i < 10; i++){
+
+  int arrLength = sizeof(test)/sizeof(test[0]);
+
+  for (int i = 0; i < 10; i++){
     test[i] = rand();
   }
-  int arrLength = sizeof(test)/sizeof(test[0]);
-  int checker = arrayPrintOut(test,arrLength);
+  printf("This is the original array \n");
+  arrayPrintOut(test,arrLength);
+
   double average = averageArray(test,arrLength);
-  checker = arrayPrintOut(test,arrLength);
-  printf("The average is %lf \n", average);
+  printf("The average of the array is %lf \n",average);
 
   copyArray(test,copy,arrLength);
-  int checker2 = arrayPrintOut(copy,arrLength);
+  printf("This is the copy of the array\n");
+  arrayPrintOut(copy,arrLength);
+
+  printf("And this is the original array again \n");
+  arrayPrintOut(test,arrLength);
   return 0;
 }
