3de19b11f0e0ec78f331934a60a7862396f7e650
KenLi8504 <kli22@stuy.edu>
2021-10-05 00:34:36 -0400

    Completed problem 2? Weird thing with adding final number

--- systems07.c
+++ systems07.c
@@ -3,14 +3,26 @@
 #include <time.h>
 
 int arrayPrintOut(int *array, int length){
-  for (int i = 0; i < length; i++){
-    printf("This element is %d \n", *(array+i));
+  printf("[");
+  for (int i = 0; i < length - 1; i++){
+    printf("%d, ", *(array+i));
   }
+  printf("%d] \n",*(array+length));
   return 0;
 }
 
+double averageArray(int *array, int length){
+  double counter = 0;
+
+  for (int i = 0; i <= length; i++){
+    printf("%int \n",*(array+i));
+    counter = counter + *(array+i);
+    printf("The current sum is %lf \n", counter);
+  }
+  return (counter/length);
+}
+
 int main(){
-  
   int test[10];
   srand(time(NULL));
   test[0] = 0;
@@ -19,5 +31,8 @@ int main(){
   }
   int arrLength = sizeof(test)/sizeof(test[0]);
   int checker = arrayPrintOut(test,arrLength);
+  double average = averageArray(test,arrLength);
+  checker = arrayPrintOut(test,arrLength);
+  printf("The average is %lf \n", average);
   return 0;
 }
